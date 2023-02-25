ec5063ef8631873721e427adeeb77533d02a80f7
toudak52 <toudak52@naver.com>
2023-02-25 18:37:34 +0900

    Feat login

--- src/main/java/com/korit/karaoke/aop/ParamsAop.java
+++ src/main/java/com/korit/karaoke/aop/ParamsAop.java
@@ -1,16 +1,11 @@
 package com.korit.karaoke.aop;
 
-import com.korit.karaoke.exception.CustomValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.BeanPropertyBindingResult;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @Aspect
@@ -25,11 +20,12 @@ public class ParamsAop {
 
         Object[] args = proceedingJoinPoint.getArgs();
 
-        for(Object arg : args) {
+        for (Object arg : args) {
             log.info("{}", arg);
         }
 
         return proceedingJoinPoint.proceed();
+
     }
 
 }
